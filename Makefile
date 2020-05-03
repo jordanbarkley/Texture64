0af3dc17a0834e22412c5d61051c29d3ad2f5785
jordanbarkley <jordan@charloe.com>
2020-05-02 20:22:37 -0400

    just terrible CI4 code

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 PROGRAM=Texture64
+TARGET=52
 
 # compile and create executable
 all:
-	javac -cp . $(PROGRAM).java
+	javac -cp . -source 1.7 -target 1.7 $(PROGRAM).java
 	jar cmf $(PROGRAM).mf $(PROGRAM).jar $(PROGRAM).class $(PROGRAM).java
 
 # clean output
@@ -11,3 +12,5 @@ clean:
 	rm -rf *.class
 	rm -rf *.rgba5551
 	rm -rf *.rgba8888
+	rm -rf *.ci4tex
+	rm -rf *.ci4pal
--- Texture64.java
+++ Texture64.java
@@ -4,6 +4,9 @@ import java.io.*;
 import java.util.Arrays;
 import java.nio.file.*;
 
+// this whole file is trash and needs to be rewritten LMAO
+// Cyjorg 5/2/2020
+
 public class Texture64 {
 
     public BufferedImage bi = null;
@@ -39,17 +42,17 @@ public class Texture64 {
         int startY = 0;
         int w = this.bi.getWidth();
         int h = this.bi.getHeight();
-        int rgbArray[] = new int[w * h];
+        int argbArray[] = new int[w * h];
         int offset = 0;
         int scansize = w;
-        bi.getRGB(startX, startY, w, h, rgbArray, offset, scansize);
+        bi.getRGB(startX, startY, w, h, argbArray, offset, scansize);
 
         // test rgba5551
-        rgba5551(rgbArray);
-        rgba8888(rgbArray);
+        rgba5551(argbArray);
+        rgba8888(argbArray);
     }
 
-    public void rgba5551(int[] rgbArray) {
+    public void rgba5551(int[] argbArray) {
         // declare variable for each channel
         int red;
         int green;
@@ -58,7 +61,7 @@ public class Texture64 {
         int argb8888;
 
         // create RGBA_5551 (16 bits, 2 bytes per pixel)
-        byte outArray[] = new byte[rgbArray.length * 2];
+        byte outArray[] = new byte[argbArray.length * 2];
 
         // holds rrrrrggg bits
         byte colorHigh;
@@ -69,9 +72,9 @@ public class Texture64 {
         // index for outArray
         int j = 0;
 
-        for (int i = 0; i < rgbArray.length; i++) {
+        for (int i = 0; i < argbArray.length; i++) {
             // get color
-            argb8888 = rgbArray[i];
+            argb8888 = argbArray[i];
 
             // get channels
             red = (getRed(argb8888) & 0xF8) >> 3;
@@ -109,6 +112,9 @@ public class Texture64 {
             j += 2;
         }
 
+        // bad place to put a funciton lol!
+        ci4(outArray);
+
         // interleave the array (O(n))
         this.interleave(outArray);
 
@@ -121,7 +127,7 @@ public class Texture64 {
         }
     }
 
-    public void rgba8888(int[] rgbArray) {
+    public void rgba8888(int[] argbArray) {
         // declare variable for each channel
         int red;
         int green;
@@ -130,14 +136,14 @@ public class Texture64 {
         int argb8888;
 
         // create RGBA_5551 (32 bits, 4 bytes per pixel)
-        byte outArray[] = new byte[rgbArray.length * 4];
+        byte outArray[] = new byte[argbArray.length * 4];
 
         // index for outArray
         int j = 0;
 
-        for (int i = 0; i < rgbArray.length; i++) {
+        for (int i = 0; i < argbArray.length; i++) {
             // get color
-            argb8888 = rgbArray[i];
+            argb8888 = argbArray[i];
 
             // get channels
             red = getRed(argb8888);
@@ -163,6 +169,104 @@ public class Texture64 {
             System.out.println("Unknown error occured!");
             System.exit(0);
         }
+    }
+
+    public void ci4(byte[] rgba5551s) {
+        // get colors, put them into an array
+        byte colorHigh;
+        byte colorLow;
+        byte[] palette = new byte[16 * 2]; 
+        boolean inPalette = false;
+        int paletteIndex = 0;
+        byte[] paletteIndexes = new byte[rgba5551s.length / 2];
+
+        // init arrays
+        for (byte i = 0; i < palette.length; i++) {
+            palette[i] = 0;
+        }
+
+        for (int i = 0; i < paletteIndexes.length; i++) {
+            paletteIndexes[i] = 0;
+        }
+
+        // omega(16n) bleh
+        for (int i = 0; i < rgba5551s.length; i += 2) {
+            // get color
+            colorHigh = rgba5551s[i];
+            colorLow = rgba5551s[i + 1];
+            
+            // check if color is already in 
+            for (int j = 0; j < palette.length; j += 2) {
+                if (colorHigh == palette[j] && colorLow == palette[j + 1]) {
+                    inPalette = true;   
+                    break;
+                }
+            }
+
+            // add to palette if necessary
+            if (!inPalette) {
+                try {
+                    palette[paletteIndex] = colorHigh;
+                    palette[paletteIndex + 1] = colorLow;
+                    paletteIndex += 2;
+                } catch (Exception e) {
+                    System.out.println("CI4 color max exceeded!");
+                    return;
+                }
+            }
+
+            // reset inPalette after adding color to palette
+            inPalette = false;
+        }
+
+        // gather indicies
+        for (int i = 0; i < paletteIndexes.length; i++) {
+            // get color
+            colorHigh = rgba5551s[i * 2];
+            colorLow = rgba5551s[i * 2 + 1];
+            
+            for (byte j = 0; j < palette.length; j += 2) {
+                if (colorHigh == palette[j] && colorLow == palette[j + 1]) {
+                    paletteIndexes[i] = (byte) (j / 2);
+                }
+            }
+        }
+
+        // debug, print paleteindexes
+        for (int i = 0; i < paletteIndexes.length; i++) {
+            //System.out.printf("paletteIndexes[%d] = %X\n", i,  paletteIndexes[i]);
+        }
+
+        // create texture
+        byte ci4Out[] = new byte[rgba5551s.length / 4];
+        for (int i = 0; i < ci4Out.length; i++) {
+            ci4Out[i] = 0;
+        }
+
+        for (int i = 0; i < paletteIndexes.length; i += 2) {
+            byte a, b;
+            a = (byte) (paletteIndexes[i] << 4);
+            b = (byte) (paletteIndexes[i + 1]);
+            ci4Out[i / 2] = (byte) (a | b); 
+        }
+        
+        // write pallette
+        try (FileOutputStream fos = new FileOutputStream("out.ci4pal")) {
+               fos.write(palette);
+        } catch (Exception e) {
+            System.out.println("Unknown error occured!");
+            System.exit(0);
+        }
+
+        // write texture
+        try (FileOutputStream fos = new FileOutputStream("out.ci4tex")) {
+               fos.write(ci4Out);
+        } catch (Exception e) {
+            System.out.println("Unknown error occured!");
+            System.exit(0);
+        }
+
+
     }
 
     public int getAlpha(int argb8888) {
